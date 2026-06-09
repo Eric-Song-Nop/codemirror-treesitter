51406e56c7ccee89903ec170a37d202969411d14
Eric-Song-Nop <ericoolen@163.com>
2026-06-09 15:33:28 +0800

    test: fix live-md-loro workspace aliases

--- packages/live-md-loro/vite.config.ts
+++ packages/live-md-loro/vite.config.ts
@@ -3,9 +3,16 @@ import { workspaceAliasSubset } from "../../vite.shared.ts";
 
 export default defineConfig({
   resolve: {
-    alias: {
-      ...workspaceAliasSubset(["@codemirror-treesitter/live-md", "loro-codemirror"]),
-    },
+    alias: workspaceAliasSubset([
+      "@codemirror-treesitter/autocomplete",
+      "@codemirror-treesitter/basic-setup",
+      "@codemirror-treesitter/commands",
+      "@codemirror-treesitter/language",
+      "@codemirror-treesitter/language-data",
+      "@codemirror-treesitter/live-md",
+      "@codemirror-treesitter/theme-gruvbox",
+      "loro-codemirror",
+    ]),
   },
   pack: {
     deps: {
